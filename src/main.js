c5269c43d0b401f1320c9948f57471289cfb0b7a
句号 <747970689@qq.com>
2020-08-26 22:19:29 +0800

    Vant初体验

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 // 引入通用样式
 import './style/base.less'
 // 引入字体图片样式
 import './style/iconfont.css'
+// 全局导入Vant（移动端的组件插件）
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 使用Vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 

06abbe5f266099d6d97c26fd98e051c76c7d63f7
句号 <747970689@qq.com>
2020-08-29 22:12:50 +0800

    设置全局前置卫士

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './style/base.less'
 // 引入字体图片样式
 import './style/iconfont.css'
 // 全局导入Vant（移动端的组件插件）
-import Vant, { Toast } from 'vant'
+import Vant from 'vant'
 import 'vant/lib/index.css'
 // 引入amfe-flexible   rem适配用的 html的fontSize会被设置为屏幕宽度的1/10
 import 'amfe-flexible'
@@ -31,16 +31,6 @@ Vue.filter('time', input => {
   return moment(input).format('YYYY-MM-DD')
 })
 
-// 导航卫士：全局前置收尾，如果跳转的页面不是user页面或者有token，就能跳转，不然就回到登录页
-router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token')
-  if (to.name !== 'user' || token) {
-    next()
-  } else {
-    Toast('未登录，请先登录')
-    router.push('login')
-  }
-})
 // 把axios对象添加到Vue的原型对象中，这样发送请求是就可以直接用原型对象中$axios，不用在各个页面分别引用
 Vue.prototype.$axios = axios
 // 给axios配置默认的baseURL，基准地址，发送请求是会自动拼接，直接写接口，也方便管理
--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,17 @@ VueRouter.prototype.push = function push (location) {
   // 原理：捕捉跳转异常，并替换原来的push方法
   return originalPush.call(this, location).catch(err => err)
 }
+
 const router = new VueRouter({
   routes
+})
+// 导航卫士：全局前置收尾，如果跳转的页面不是user页面或者有token，就能跳转，不然就回到登录页
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.name !== 'user' || token) {
+    next()
+  } else {
+    router.push('login')
+  }
 })
 export default router
